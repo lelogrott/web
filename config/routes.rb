7587690de47f2491f2f8fb3ded1b4efbf3a05ad6
Krowck <gabrieldominicoo@gmail.com>
2017-06-25 18:29:46 -0300

    form para criar receita ok

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 	get '/logout', to: 'sessions#destroy'
 
 	resources :usuarios
+	resources :receitas
 
 	#match '/usuario/create' => 'usuario#create', :as => :create_usuario_path, :via => :post
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
