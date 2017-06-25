674dae1c1a0864736fe82aa7a5668869f12b5f52
Krowck <gabrieldominicoo@gmail.com>
2017-06-25 10:55:06 -0300

    removed model Receitum

--- app/models/receitum.rb
+++
@@ -1,2 +0,0 @@
-class Receitum < ApplicationRecord
-end
--- db/migrate/20170625011553_create_receita.rb
+++
@@ -1,8 +0,0 @@
-class CreateReceita < ActiveRecord::Migration[5.1]
-  def change
-    create_table :receita do |t|
-
-      t.timestamps
-    end
-  end
-end
