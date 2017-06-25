91e5c37a78b68ed25d1c63d07913ac8f9d31b37b
Krowck <gabrieldominicoo@gmail.com>
2017-06-25 19:23:23 -0300

    view criada com sucesso

--- app/helpers/receitas_helper.rb
+++ app/helpers/receitas_helper.rb
@@ -1,2 +1,15 @@
 module ReceitasHelper
+	include ActionView::Helpers::TagHelper
+
+	def tabela_de_ingredientes(receita)
+		ingredientes = receita.ingredientes
+		content_tag :table do
+			ingredientes.each do |ingrediente|
+				concat(content_tag(:tr,
+					content_tag(:td, IngredientesReceita.find_by(receita_id: receita.id, ingrediente_id: ingrediente.id).quantidade) +
+					content_tag(:td, ingrediente.name)
+				)).to_s.html_safe
+			end
+		end
+	end
 end
