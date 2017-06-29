257cf94a7602046f3c5442a739094aed4f8d7f1a
Krowck <gabrieldominicoo@gmail.com>
2017-06-29 04:50:20 -0300

    ok

--- app/helpers/receitas_helper.rb
+++ app/helpers/receitas_helper.rb
@@ -3,7 +3,7 @@ module ReceitasHelper
 
 	def tabela_de_ingredientes(receita)
 		ingredientes = receita.ingredientes
-		content_tag :table do
+		content_tag :table, class: 'ingredients-table' do
 			ingredientes.each do |ingrediente|
 				concat(content_tag(:tr,
 					content_tag(:td, IngredientesReceita.find_by(receita_id: receita.id, ingrediente_id: ingrediente.id).quantidade) +
